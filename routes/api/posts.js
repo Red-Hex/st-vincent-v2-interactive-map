1a14519e6bf6c02c0420e4a947866be15ba65e94
RedHex <55835487+Red-Hex@users.noreply.github.com>
2020-12-01 23:43:49 +0000

    Added Post Creation and Deletion

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -19,20 +19,21 @@ router.post('/', [auth, [
             check('longitude', 'Enter longitude')
                 .exists({ checkFalsy: true}),
             check('lattitude', 'Enter lattiude')
+                .exists({ checkFalsy: true })
         ]
     ],
     async (req, res) => {
         const errors = validationResult(req);
         if(!errors.isEmpty()) {
-            return res.status(400).json({ errors: error.array() });
+            return res.status(400).json({ errors: errors.array() });
         }
         try {
             const newPost = new Post({
-                name: req.body.text, 
-                category_id: req.body.text,
-                description: req.body.text,
-                longitude: req.body.number,
-                lattitude: req.body.number
+                name: req.body.name, 
+                category_id: req.body.category_id,
+                description: req.body.description,
+                longitude: req.body.longitude,
+                lattitude: req.body.lattitude
             });
             const post = await newPost.save();
 
@@ -44,6 +45,66 @@ router.post('/', [auth, [
     }
 
 );
+
+// @route  GET api/posts/:id
+// @desc   Get a post by id
+// @access Private
+
+router.get('/', auth, async (req, res) => {
+    try {
+        const posts = await Post.find().sort({ date: -1 });
+        res.json(posts);
+    } catch (err) {
+        console.error(err.message);
+        res.status(500).send('Server Error');
+    }
+});
+
+// @route  GET api/posts/:id
+// @desc   Get a post by id
+// @access Private
+
+router.get('/:id', auth, async (req, res) => {
+    try {
+        const post = await Post.findById(req.params.id);
+
+        if(!post) {
+            return res.status(404).json({ msg: 'Post not found'});
+        }
+
+        res.json(post);
+    } catch (err) {
+        console.error(err.message);
+        if(err.kind === 'ObjectId') {
+            return res.status(404).json({ msg: 'Post not found' });
+        }
+        res.status(500).send('Server Error');
+    }
+});
+
+// @route  DELETE api/posts/:id
+// @desc   Delete a map post
+// @access Private
+
+router.delete('/:id', auth, async (req, res) => {
+    try {
+        const post = await Post.findById(req.params.id);
+
+        if(!post) {
+            return res.status(401).json({ msg: 'User not Authorised '});
+        }
+        
+        await post.remove();
+
+        res.json({ msg: 'Post has been Removed'});
+    } catch {
+        console.error(err.message);
+        if(!post) {
+            return res.status(404).json({ msg: 'Post not found'});
+        }
+        res.status(500).send('Server Error');
+    }
+})
 
 module.exports = router;
 
