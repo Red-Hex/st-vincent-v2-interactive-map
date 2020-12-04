9ddda8a22fcad7799f2db30a32f31e48d21e2541
RedHex <55835487+Red-Hex@users.noreply.github.com>
2020-12-04 21:43:17 +0000

    Get data from posts api

--- client/src/components/Admin.js
+++ client/src/components/Admin.js
@@ -1,11 +1,22 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
 import { Link } from 'react-router-dom';
-
+import axios from 'axios';
 
 import "../css/Header.css";
 import "../css/Admin.css";
 
 const Admin = ()  => {
+    const [posts, setPosts] = useState([]);
+
+    useEffect(() => {
+        axios.get('http://localhost:5000/api/posts')
+
+        .then((res) => {
+            setPosts(res.data);
+        });
+    }, []);
+
+    
     return (
         <div class="admin">
             <React.Fragment>
@@ -15,6 +26,29 @@ const Admin = ()  => {
                 <h2 class="title">Admin Panel</h2>
                 <p className='backButton'>Create Post</p>
                 <h2 class="title">Posts</h2>
+                <table className='post-container'>
+                    <thead>
+                        <tr>
+                            <th>Post Name</th>
+                            <th>Lattitude</th>
+                            <th>Longitude</th>
+                            <th>Actions</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {posts.map((post) => {
+                            return (
+                                <tr className='project' key={post.id}>
+                                    <td>{post.name}</td>
+                                    <td>{post.lattitude}</td>
+                                    <td>{post.longitude}</td>
+                                    <td></td>
+                                </tr>
+                            )
+                        })}
+                    </tbody>
+                   
+                </table>
             </React.Fragment>
         </div>
     )
